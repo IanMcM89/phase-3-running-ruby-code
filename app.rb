824e66238660aaa443ea7a1c4235847b504e0ec2
Ian <ianmcm.89@gmail.com>
2022-01-16 11:56:28 -0500

    completed assignment

---
+++ app.rb
@@ -0,0 +1,12 @@
+# app.rb puts
+puts "Hello World!"
+
+# app.rb print
+print "Pass this test, please."
+
+# This is the equivalent of calling:
+# puts [1, 2, 3].inspect
+p [1, 2, 3]
+
+# app.rb
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
